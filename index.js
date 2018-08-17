3a5758c7720902685f0d6e54f8072c21475cb628
KID <kid.wumeng@gmail.com>
2018-08-17 18:56:43 +0800

    fixed bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('coffeescript/register')
-require('colors')
-_ = require('lodash')
+// require('colors')
+// _ = require('lodash')
 
 
 
@@ -8,23 +8,15 @@ module.exports = sai = require('./lib')
 
 
 
-;(async function(){
-
-   try {
-
-
-      data = await sai.readJSON('test/file/_assets/test2.json')
-      console.log(data);
-
-
-   } catch (error) {
-      // console.log(error.name.red)
-      console.log(error.message.red)
-   }
-
-})()
-
-
-
-
-
+// ;(async function(){
+//
+//    try {
+//
+//
+//
+//    } catch (error) {
+//       // console.log(error.name.red)
+//       console.log(error.message.red)
+//    }
+//
+// })()
